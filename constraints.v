a7ba157e1d48b278f59bc2fa5d26a12a1bb5e7ea
Geddada santosh <geddadisantosh58@gmail.com>
2026-04-30 21:30:47 +0530

    Add timing constraints for clock and signals

---
+++ constraints.v
@@ -0,0 +1,8 @@
+create_clock -name clk -period 10 -waveform {0 5} [get_ports clk]
+set_clock_uncertainty 0.2 [get_clocks clk]
+set_clock_transition 0.1 [get_clocks clk]
+set_input_delay 2.0 -clock clk [remove_from_collection [all_inputs] [get_ports clk]]
+set_output_delay 2.0 -clock clk [all_outputs]
+set_driving_cell -lib_cell INVX1 [all_inputs]
+set_load 0.05 [all_outputs]
+set_false_path -from [get_ports reset]
